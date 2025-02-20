9ac6840f2ed767be42929307ca268f59fc07257d
safrizalrahman46 <safrizalrahman46@gmail.com>
2025-02-20 13:08:44 +0700

    Push Praktikum Pertama Route::get('/world', function () {

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,12 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/hello', function () {
-    return 'Hello World'; });
+    return 'Hello World';
+});
+Route::get('/world', function () {
+    return 'World';
+});
+
 // Route::get('/', function () {
 //     return view('welcome');
 // });
